cc00f16f1c01219024c66214dac29617929e09f4
SongTan <165980942+SepineTam@users.noreply.github.com>
2024-09-01 17:19:35 +0800

    Update _config.demo.py

--- _config.demo.py
+++ _config.demo.py
@@ -1,3 +1,11 @@
 SRC_BASE = "demo_src/mp/"
 WORDS_BASE = "./demo_src/words/word.txt"
-loca = "uk"  # or you can choose us as American English
+
+pron_loca = "uk"  # or you can choose "us" as American English
+
+is_play = False
+
+is_check = False
+check_position = "before"  # or you could change it to after
+
+self_destruct = False  # 阅后即焚，清除占用空间
